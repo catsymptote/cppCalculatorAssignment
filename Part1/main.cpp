adf7b4e1eaf76d8c9cacb9377c2cdee082fd3187
catsymptote <catsymptote@gmail.com>
2017-09-30 21:41:44 +0200

    Added window open feature.

--- Part1/main.cpp
+++ Part1/main.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
+#include <conio.h> //for getch()
+
 
 double operatorFunction(double num1, double num2, char oper);
 
 
 int main()
 {
+    // Number 1
     std::cout << "Number 1> ";
-    double num1;   // Number 1
+    double num1;
     std::cin >> num1;
 
+    // Operator
     std::cout << "Operator> ";
-    char oper;  // Operator
+    char oper;
     std::cin >> oper;
 
+    // Number 2
     std::cout << "Number 2> ";
-    double num2;   // Number 2
+    double num2;
     std::cin >> num2;
 
+    // Print solution
     std::cout << "Input:\t" << num1 << " " << oper << " " << num2 << std::endl;
     std::cout << "Result:\t" << operatorFunction(num1, num2, oper) << std::endl;
+
+    // Holding window open
+    std::cout << "Press any key to close ";
+    getch();
 
     return 0;
 }
